61987ed0d025a9826a03eff3443dd01fe8b0e810
Alexandre Castro <im-alexandre07@gmail.com>
2022-01-07 16:16:59 -0300

    Primeiro commit

--- exercicios/express/ex01.js
+++ exercicios/express/ex01.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const server = express()
 
-server.get('/', function(req, res) {
-  res.send('<h1>Index!</h1>')
+server.post('/', function(req, res) {
+    console.log(req.headers)
+    res.send('<h1>Index!</h1>')
 })
 
 server.all('/teste', function(req, res) {
-  res.send('<h1>Teste!</h1>')
+    res.send('<h1>Teste!</h1>')
 })
 
 server.get(/api/, function(req, res) {
-  res.send('<h1>API!</h1>')
+    res.send('<h1>API!</h1>')
 })
 
 server.listen(3000, () => console.log('Executando...'))
--- exercicios/node/ex11_server.js
+++ exercicios/node/ex11_server.js
@@ -1,6 +1,9 @@
 const http = require('http')
 const server = http.createServer(function(req, res) {
-  res.writeHead(200, {"Content-Type": "text/html"})
+  //res.writeHead(200, {"Content-Type": "text/json"})
+  //res.end("{teste: 'teste'}")
+
+  res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
   res.end('<h1>Acho que é melhor usar o Express, não?</h1>')
 })
 
